82af8f9ec8e27ca2f41115473b92b56a3e088bde
Yana Anatska <69202027+rozhnovaYana@users.noreply.github.com>
2024-01-15 15:08:09 +0200

    added content

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,4 +1,52 @@
-const CommunityPage = () => {
-  return <div>Community page</div>;
-};
-export default CommunityPage;
+import Image from "next/image";
+import classes from "./page.module.css";
+
+const perks = [
+  {
+    image: "/assets/icons/meal.png",
+    text: "Share & discover recipes",
+    alt: "A delicious meal",
+  },
+  {
+    image: "/assets/icons/community.png",
+    text: "Find new friends & like-minded people",
+    alt: "A crowd of people, cooking",
+  },
+  {
+    image: "/assets/icons/events.png",
+    text: "Participate in exclusive events",
+    alt: "A crowd of people at a cooking event",
+  },
+];
+
+export default function CommunityPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          One shared passion: <span className={classes.highlight}>Food</span>
+        </h1>
+        <p>Join our community and share your favorite recipes!</p>
+      </header>
+      <main className={classes.main}>
+        <h2>Community Perks</h2>
+
+        <ul className={classes.perks}>
+          {perks.map((perk, ind) => {
+            return (
+              <li key={ind}>
+                <Image
+                  src={perk.image}
+                  alt={perk.alt}
+                  width={100}
+                  height={100}
+                />
+                <p>{perk.text}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
+    </>
+  );
+}
