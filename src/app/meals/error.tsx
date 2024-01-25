c85f94b6cf3864dc88ac954382afdf3631fc0ce3
Yana Anatska <69202027+rozhnovaYana@users.noreply.github.com>
2024-01-25 17:54:47 +0200

    added error handler

---
+++ src/app/meals/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+export default function Error() {
+  return (
+    <div className="error">
+      <h1>Something went wrong</h1>
+      <p>Please, try again later</p>
+    </div>
+  );
+}
